ab799eeea96fa2f65b0c5cf4dd2b5535a93b986e
Jacklyn-T <jacklynt@bu.edu>
2022-11-29 17:30:54 -0500

    initial

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // put your globals here - hint: select elements from the HTML
+const textarea = document.querySelector(".form-control");
+const countBtn = document.getElementById("countBtn");
+const findWords = document.getElementById("findWordInput");
+const wordCountInput = document.getElementById("wordCountInput");
+const str = string(textarea);
+const words = textarea.split(' ');
 
 
+countBtn.addEventListener("click", countWords);
+findWords.addEventListener("click",findWords);
 // dont' forget to add event listeners to teh buttons
 
 function countWords() {
   // your code here
+  wordCountInput.value = (
+    words.length);
   // call printData
 }
 
@@ -17,4 +27,14 @@ function findWords() {
 // change param1 and param2 to identifiers that make sense
 function printData(param1, param2) {
   // your code here... one line!
+}
+
+
+
+
+
+
+function countWords(){
+  
+
 }
