aaae70cba68dfea686399fce5675dadf9a8960ca
Jacklyn-T <jacklynt@bu.edu>
2022-11-30 20:58:53 -0500

    final

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 // put your globals here - hint: select elements from the HTML
-const textarea = document.querySelector(".form-control");
-const countBtn = document.getElementById("countBtn");
-const findWords = document.getElementById("findWordInput");
-const wordCountInput = document.getElementById("wordCountInput");
-const str = string(textarea);
-const words = textarea.split(' ');
+const textInput = document.getElementById("text-area");
+const wordcountBtn = document.getElementById("countBtn");
+const wordCountResult = document.getElementById("wordCountInput");
+const searchWord = document.getElementById("findWordInput");
+const findBtn = document.getElementById("findBtn");
+const numberWordsFound = document.getElementById("wordFoundInput");
 
 
 countBtn.addEventListener("click", countWords);
-findWords.addEventListener("click",findWords);
+findBtn.addEventListener("click", findWords);
 // dont' forget to add event listeners to teh buttons
 
 function countWords() {
   // your code here
-  wordCountInput.value = (
-    words.length);
+  let listofWords = textInput.value.split(" ");
+  const total = listofWords.length;
+  wordCountResult.value = total;
   // call printData
 }
 
@@ -22,19 +23,18 @@ function findWords() {
   // put your local variable for the empty array here
   // remainder of your code follows
   // call printData
+  let listofWords = textInput.value.split(" ");
+  let searchedWord = searchWord.value;
+  count = 0;
+    for (let i = 0; i < listofWords.length; i++) {
+      if (listofWords[i] == searchedWord) {
+        count = count + 1;
+      }
+    }
+    numberWordsFound.value = count;
 }
 
 // change param1 and param2 to identifiers that make sense
 function printData(param1, param2) {
   // your code here... one line!
-}
-
-
-
-
-
-
-function countWords(){
-  
-
 }
